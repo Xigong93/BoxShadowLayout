e99bb5930287286602f334dc12c33710af73027f
pokercc <pokercc@sina.com>
2020-10-20 20:25:16 +0800

    Draw color

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BoxShadowLayout
  Box Shadow like css in web.
+## Feature
+- Shadow outside of View, setParent clipChild = false.
--- app/src/main/java/pokercc/android/boxshadowlayout/BoxShadowLayout.kt
+++ app/src/main/java/pokercc/android/boxshadowlayout/BoxShadowLayout.kt
@@ -1,7 +1,9 @@
 package pokercc.android.boxshadowlayout
 
 import android.content.Context
+import android.graphics.*
 import android.util.AttributeSet
+import android.view.ViewGroup
 import android.widget.FrameLayout
 
 /**
@@ -9,6 +11,10 @@ import android.widget.FrameLayout
  */
 class BoxShadowLayout(context: Context, attrs: AttributeSet? = null) : FrameLayout(context, attrs) {
 
+
+    init {
+        setWillNotDraw(false)
+    }
 
     private fun init(attrs: AttributeSet?, defStyle: Int) {
         // Load attributes
@@ -19,5 +25,42 @@ class BoxShadowLayout(context: Context, attrs: AttributeSet? = null) : FrameLayo
         }.recycle()
 
     }
+
+    private val shadowColor = 0xffc7ccde.toInt()
+    private val shadowPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = 0xffc7ccde.toInt()
+        maskFilter = BlurMaskFilter(5f, BlurMaskFilter.Blur.INNER)
+        shader = LinearGradient(
+            0f,
+            0f,
+            300f,
+            0f,
+            shadowColor,
+            0xFFFF7D39.toInt(),
+            Shader.TileMode.MIRROR
+        )
+        this.strokeWidth = shadowWidth
+        this.style = Paint.Style.FILL
+    }
+    private val shadowWidth = 15f
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        // Tell parent don't clip me. Otherwise the shadow will be erase.
+        (parent as? ViewGroup)?.clipChildren = false
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawRoundRect(
+            shadowWidth * -1,
+            shadowWidth * -1,
+            width + shadowWidth,
+            height + shadowWidth,
+            0f,
+            0f,
+            shadowPaint
+        )
+    }
+
 
 }
